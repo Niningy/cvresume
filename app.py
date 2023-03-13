41c4ea4a1bd7ad92ece78e5d8d729703f7eefa28
Niningy <116931859+Niningy@users.noreply.github.com>
2023-03-13 13:36:40 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import numpy as np
+import PIL
+import pathlib
+
+# ---PATH SETTING---
+
+#################
+# header
+st.write('''
+# Nining Yuningsih
+#### 
+''')
+
+st.image('profile.png',  width = 200)
+# header
+st.markdown('*Customervservices, Administration, receptionist, Adm.Hr, Adm.Logistic, Adm.Operational, Digital Sales*', unsafe_allow_html=True)
+st.markdown('## Media Social', unsafe_allow_html=True)
+st.info('''
+- Email: niningy383@gmail.com
+- Phone:+6285817253707
+- Whatsapp: https://wa.me/+6285817253707
+- Linkedin:https://Linkedin.com
+- Github  :https://github.com
+- tWitter :https://twitter.com
+    ''')
+
+st.markdown('## ADUCATION', unsafe_allow_html=True)
+st.info('''
+- Universitas Tebuka(UT) Bachelor of Accounting 2018-2023
+- High School SMA PANDU IPA Passed 2011
+- MTS TSANAWIAH Passed 2008
+- MI SIRAJUL HUDA Passed 2007
+''')
+
+st.markdown('## NON FORMAL EDUCATION', unsafe_allow_html=True)
+st.info('''
+- Training Course Computer High School
+- Chief of Kadiv.Progrram Seminar Education Youth Meeting
+- English Club
+- TOEFL
+- Training Course online SAP MM and SAP Netweaver ABAP Program
+- Training Course Python Progrraming
+''')
+
+
+st.markdown('## JOB EXPERIENCES', unsafe_allow_html=True)
+st.info('''
+- Briton School Of English (BSE) Course and Tranlation Bording Position Administration - South Jakarta 2012-2013
+- PT.Garuda Ekspress Delivary (Ekspedition Company) Customer services & Sales Outlate Position South Jakarta 2013-2014
+- PT.Carefastindo Last Position Administrasi Support Logistic West Jakarta 2014-2020
+- PT.HIJAU BERDIKARI INDONESIA last Position Administrasi Opeerational
+- Founder Wijayakusumasport online shop umkm Bogor West Java 2020-2021
+- Feelancer Sales\Marketing Online 2021-Now
+- Freelancer Tutor in the PKBM Bording School 2022-Now
+''')
+
+st.markdown('## Skill', unsafe_allow_html=True)
+st.info('''
+- Language: Engllish 
+- Programming: Python
+- Data processing: SQL, Pandas, numpy
+- Data Visualizatio: matplotlib, plotly, seabron
+- Model deployment : streamlit, Heroku
+''')
+st.markdown('## Hobbi', unsafe_allow_html=True)
+st.info('''
+- Play Music Piano & Guitar
+''')
+
+st.markdown('## Certificates & Awards', unsafe_allow_html=True)
+st.info('''
+- Certificate Computer program(Ms.Word,Ms.Ecxel,Ms.Power Point
+- Certificate TOEFL of Eligmu Course
+- Certificate Royalti Award 2019 of PT.Carefastindo & Congratulation Achieve Business of PT.Carefastindo
+- Certificate SAP MM of BelajarSAP course
+- Certificate Auditor 3 SKP of Auditor Indonesia
+- Certificate networking Academy(Cybersecurity and Cyber Esentials(CISO)
+- Certificate Akuntansi 2 SKP of open University indonesia(Universitas Terbuka(UT)
+- Certificate call paper of the P3EI(Pusat Pengkajian Dan Pengembangan Ekonomi Islam
+- Certificate training UMKM class start up Biz-UMKM(Usaha Kecil Menengah
+''')
+
+
+
